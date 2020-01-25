71b27eea48e8a4a135faf09be2b7840f6bbeaa5c
Akshay Deshpande <akshay@LAPTOP-PFLH5TE1.localdomain>
2020-01-25 22:25:38 +1000

    added CRUD for cards

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
 var db = require('./db')
-const port = 3000
+const port = 10000
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
@@ -24,19 +24,8 @@ app.use(function(req, res, next) {
   next(createError(404));
 });
 
-// // error handler
-// app.use(function(err, req, res, next) {
-//   // set locals, only providing error in development
-//   res.locals.message = err.message;
-//   res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-//   // render the error page
-//   res.status(err.status || 500);
-//   res.render('error');
-// });
-
 app.listen(port, function () {
-  console.log('Example app listening on port 3000!')
+  console.log(`Example app listening on port ${port}`)
 })
 
 module.exports = app;
--- controllers/card.js
+++ controllers/card.js
@@ -1,13 +1,30 @@
 var Card = require('../models/card')
 
-module.exports.createCard = (cardData) => {
-    const card = new Card(cardData)
+module.exports.createCard = (req, res) => {
+    const card = new Card(req.body)
     // All db operatons are async and need to supply a callback function
-    return card.save(function (err) {
-        if (err) console.log('Error with ' + cardData)
+    card.save(function (err, card) {
+        if (err) console.log(`Error with data ${req.body}`)
+        res.sendStatus(200) 
     })
 }
 
 module.exports.getAllCards = (req, res) => {
-    Card.find({}, function (err, data) {res.send(data)});
+    Card.find({}, function (err, cards) {
+        res.send(cards)
+    });
+}
+
+module.exports.editCard = (req, res) => {
+    Card.findByIdAndUpdate(req.params.id, req.body, function (err, card){ 
+        if (err) console.log(`Error with data ${err}`)
+        else res.sendStatus(200)
+    })
+}
+
+module.exports.getCard = (req, res) => {
+    Card.findById(req.params.id, function (err, card){
+        if (err) console.log(`Error with data ${err}`)
+        else res.send(card)
+    })
 }
